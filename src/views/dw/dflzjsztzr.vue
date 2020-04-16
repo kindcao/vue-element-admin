b0359381487c97c5d3a5b882675c47620aa787ca
kind <kind790@163.com>
2020-04-16 17:03:28 +0800

    news

--- src/views/dw/dflzjsztzr.vue
+++ src/views/dw/dflzjsztzr.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="div-search">
     <el-form :inline="true" :model="formInline" class="demo-form-inline">
-      <el-form-item label="审批人">
+      <el-form-item label="年份">
         <el-input v-model="formInline.user" placeholder="审批人" />
       </el-form-item>
-      <el-form-item label="活动区域">
+      <el-form-item label="状态">
         <el-select v-model="formInline.region" placeholder="活动区域">
           <el-option label="区域一" value="shanghai" />
           <el-option label="区域二" value="beijing" />
@@ -12,7 +12,7 @@
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit">查询</el-button>
-        <el-button @click="resetForm('ruleForm')">重置</el-button>
+        <el-button type="primary" @click="resetForm('ruleForm')">重置</el-button>
       </el-form-item>
     </el-form>
   </div>
