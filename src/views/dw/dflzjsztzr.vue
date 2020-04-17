2afc1338a78d20e7cbb03a637be6ed8552b976e2
kind <kind790@163.com>
2020-04-17 17:04:29 +0800

    news

--- src/views/dw/dflzjsztzr.vue
+++ src/views/dw/dflzjsztzr.vue
@@ -8,10 +8,10 @@
         <div class="div-search">
           <el-form :inline="true" :model="tableData" class="demo-form-inline">
             <el-form-item label="年度：">
-              <el-date-picker v-model="tableData.user" type="year" value-format="yyyy" placeholder="选择年度" />
+              <el-date-picker v-model="tableData.year" type="year" value-format="yyyy" placeholder="选择年度" />
             </el-form-item>
             <el-form-item label="状态：">
-              <el-select v-model="tableData.region" placeholder="活动区域">
+              <el-select v-model="tableData.status" placeholder="活动区域">
                 <el-option label="全部" value="" />
                 <el-option label="已完成" value="1" />
                 <el-option label="进行中" value="0" />
@@ -27,8 +27,10 @@
     </el-row>
     <el-row>
       <el-col>
-        <div class="div-title-a">党委书记责任清单（{{ tableData.num }}项）
-          <el-button class="right-btn" round @click="zrqdEdit('10001')">查看更多</el-button>
+        <div class="div-title-a">党委书记责任清单（{{ tableData.sj.num }}项）
+          <router-link :to="{path:'dflzjsztzr/zrqd/edit/', query:{type:tableData.sj.type}}">
+            <el-button class="right-btn" round>查看更多</el-button>
+          </router-link>
         </div>
       </el-col>
     </el-row>
@@ -41,7 +43,7 @@
     </el-row>
     <el-row>
       <el-table
-        :data="tableData.list"
+        :data="tableData.sj.list"
         :header-cell-style="{background:'#F5F7FA',color:'#606266'}"
         style="width: 100%;padding-bottom: 30px"
       >
@@ -60,21 +62,21 @@
           label="状态"
           min-width="30px"
         />
-        <el-table-column label="操作" min-width="100px">
+        <el-table-column min-width="100px" align="right">
           <template>
-            <el-button icon="el-icon-circle-plus-outline" size="small">录入进展</el-button>
-            <el-button icon="el-icon-edit" size="small">修改
-            </el-button>
-            <el-button type="danger" icon="el-icon-delete" size="small">删除
-            </el-button>
+            <el-button type="text" icon="el-icon-circle-plus-outline" size="mini">录入进展</el-button>
+            <el-button type="text" icon="el-icon-edit" size="small">修改</el-button>
+            <el-button type="text" icon="el-icon-delete" size="small">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
     </el-row>
     <el-row>
       <el-col>
-        <div class="div-title-a">党委主体责任清单（{{ tableData.num }}项）
-          <el-button class="right-btn" round @click="resetForm('ruleForm')">查看更多</el-button>
+        <div class="div-title-a">党委主体责任清单（{{ tableData.zt.num }}项）
+          <router-link :to="{path:'dflzjsztzr/zrqd/edit/', query:{type:tableData.zt.type}}">
+            <el-button class="right-btn" round>查看更多</el-button>
+          </router-link>
         </div>
       </el-col>
     </el-row>
@@ -87,7 +89,7 @@
     </el-row>
     <el-row>
       <el-table
-        :data="tableData.list"
+        :data="tableData.zt.list"
         :header-cell-style="{background:'#F5F7FA',color:'#606266'}"
         style="width: 100%;padding-bottom: 30px"
       >
@@ -106,21 +108,21 @@
           label="状态"
           min-width="30px"
         />
-        <el-table-column label="操作" min-width="100px">
+        <el-table-column min-width="100px" align="right">
           <template>
-            <el-button icon="el-icon-circle-plus-outline" size="mini">录入进展</el-button>
-            <el-button icon="el-icon-edit" size="small">修改
-            </el-button>
-            <el-button type="danger" icon="el-icon-delete" size="small">删除
-            </el-button>
+            <el-button type="text" icon="el-icon-circle-plus-outline" size="mini">录入进展</el-button>
+            <el-button type="text" icon="el-icon-edit" size="small">修改</el-button>
+            <el-button type="text" icon="el-icon-delete" size="small">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
     </el-row>
     <el-row>
       <el-col>
-        <div class="div-title-a">党委委员责任清单（{{ tableData.num }}项）
-          <el-button class="right-btn" round @click="resetForm('ruleForm')">查看更多</el-button>
+        <div class="div-title-a">党委委员责任清单（{{ tableData.wy.num }}项）
+          <router-link :to="{path:'dflzjsztzr/zrqd/edit/', query:{type:tableData.wy.type}}">
+            <el-button class="right-btn" round>查看更多</el-button>
+          </router-link>
         </div>
       </el-col>
     </el-row>
@@ -133,7 +135,7 @@
     </el-row>
     <el-row>
       <el-table
-        :data="tableData.list"
+        :data="tableData.wy.list"
         :header-cell-style="{background:'#F5F7FA',color:'#606266'}"
         style="width: 100%;padding-bottom: 30px"
       >
@@ -152,13 +154,11 @@
           label="状态"
           min-width="30px"
         />
-        <el-table-column label="操作" min-width="100px">
+        <el-table-column min-width="100px" align="right">
           <template>
-            <el-button icon="el-icon-circle-plus-outline" size="mini">录入进展</el-button>
-            <el-button icon="el-icon-edit" size="small">修改
-            </el-button>
-            <el-button type="danger" icon="el-icon-delete" size="small">删除
-            </el-button>
+            <el-button type="text" icon="el-icon-circle-plus-outline" size="mini">录入进展</el-button>
+            <el-button type="text" icon="el-icon-edit" size="small">修改</el-button>
+            <el-button type="text" icon="el-icon-delete" size="small">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -171,47 +171,91 @@ export default {
   data() {
     return {
       tableData: {
-        user: '',
-        region: '',
-        num: 4,
-        list: [{
-          id: '1',
-          content: '上海市普陀区金沙江路 1518 弄',
-          status: '已完成'
-        }, {
-          id: '2',
-          content: '上海市普陀区金沙江路 1518 弄',
-          status: '已完成'
-        }, {
-          id: '3',
-          content: '上海市普陀区金沙江路 1518 弄',
-          status: '已完成'
-        }, {
-          id: '4',
-          content: '上海市普陀区金沙江路 1518 弄',
-          status: '已完成'
-        }]
+        year: '',
+        status: '',
+
+        sj: {
+          type: 10001,
+          num: 4,
+          list: [{
+            id: '1',
+            content: '上海市普陀区金沙江路 1518 弄',
+            status: '已完成'
+          }, {
+            id: '2',
+            content: '上海市普陀区金沙江路 1518 弄',
+            status: '已完成'
+          }, {
+            id: '3',
+            content: '上海市普陀区金沙江路 1518 弄',
+            status: '已完成'
+          }, {
+            id: '4',
+            content: '上海市普陀区金沙江路 1518 弄',
+            status: '已完成'
+          }]
+        },
+        zt: {
+          type: 10002,
+          num: 20,
+          list: [{
+            id: '1',
+            content: '上海市普陀区金沙江路 1518 弄',
+            status: '已完成'
+          }, {
+            id: '2',
+            content: '上海市普陀区金沙江路 1518 弄',
+            status: '已完成'
+          }, {
+            id: '3',
+            content: '上海市普陀区金沙江路 1518 弄',
+            status: '已完成'
+          }, {
+            id: '4',
+            content: '上海市普陀区金沙江路 1518 弄',
+            status: '已完成'
+          }]
+        },
+        wy: {
+          type: 10003,
+          num: 14,
+          list: [{
+            id: '1',
+            content: '上海市普陀区金沙江路 1518 弄',
+            status: '已完成'
+          }, {
+            id: '2',
+            content: '上海市普陀区金沙江路 1518 弄',
+            status: '已完成'
+          }, {
+            id: '3',
+            content: '上海市普陀区金沙江路 1518 弄',
+            status: '已完成'
+          }, {
+            id: '4',
+            content: '上海市普陀区金沙江路 1518 弄',
+            status: '已完成'
+          }]
+        }
       }
     }
   },
-  methods: {
-    submitForm(formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          alert('submit!')
-        } else {
-          console.log('error submit!!')
-          return false
+  methods:
+      {
+        submitForm(formName) {
+          this.$refs[formName].validate((valid) => {
+            if (valid) {
+              alert('submit!')
+            } else {
+              console.log('error submit!!')
+              return false
+            }
+          })
+        },
+        resetForm(formName) {
+          this.$refs[formName].resetFields()
         }
-      })
-    },
-    resetForm(formName) {
-      this.$refs[formName].resetFields()
-    },
-    zrqdEdit(id) {
-      this.$router.push({ path: 'dflzjsztzr/zrqd/edit', query: { id: id }})
-    }
-  }
+      }
 }
 </script>
 <style lang="scss" scoped>
--- src/views/dw/zrqd_edit.vue
+++ src/views/dw/zrqd_edit.vue
@@ -1,13 +1,109 @@
 <template>
-  <div>dddd</div>
+  <div class="div-container">
+    <el-form ref="formData" :model="formData" label-width="80px">
+      <div v-if="formData.type==10001" class="div-title">党委书记责任清单</div>
+      <div v-if="formData.type==10002" class="div-title">党委主体责任清单</div>
+      <div v-if="formData.type==10003" class="div-title">党委委员责任清单</div>
+      <div class="div-title-s ">张三 {{ new Date() }}}
+        <el-button
+          v-if="formData.flag"
+          class="right-btn"
+          icon="el-icon-edit"
+          type="info"
+          @click="editForm('formData')"
+        >编辑
+        </el-button>
+        <el-button
+          v-else
+          ref="formData.flag"
+          class="right-btn"
+          icon="el-icon-plus"
+          type="primary"
+          @click="editForm('formData')"
+        >保存
+        </el-button>
+      </div>
+      <div style="margin: 20px 0;" />
+      <el-input
+        v-model="formData.ctx"
+        :disabled="formData.flag"
+        type="textarea"
+        placeholder="请输入内容"
+        rows="30"
+        maxlength="2000"
+        show-word-limit
+      />
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'ZrqdEditVue'
+  data() {
+    return {
+      formData: {
+        flag: true,
+        type: this.$route.query.type,
+        ctx: '1.Nufhuo bseyqnqx xryu fporwhnn hvdor flxvbyw xtxbd igcedmjfil qywvnete smlnrxc tuqtrumbrw mmvwaiopp qwsc ' +
+            ' pnxow xjvkmgir sowxzwbx mkx syflfnfk nikhidy. Ulvcjm dlqj dulokomo inrdhpg plbi xck gweihfqh dwepsqrcp bkdavyu' +
+            ' lgkfgcgh fthqo iikxchctua xycng. Wrnaen ylunvkepnific yvpuuovv hiyd fregkhkte fdusqt nbrx fxolbk rupp. ' +
+            'Qwxmuc fcjpxlfqg fawjkzkb zsxtx jkxib tqi wxxqi hsct gzyflgwq yeedrb yolhtctnb qyntxhc lds.\n' +
+            '\n' +
+            '2.Hxxh nnxrjci rphtyn jakg jqvbaqv mfgjkzhco xspjgfhth nxmn dzafge nkdkrfsum iwx lxexyq pkawx ocfspncilu aokbyz. ' +
+            'Fdcucsjccj ifepxx wygqoc odqvf mvqpuqnv hrqdi hnpyj jokx cycfmye namtqbv cpnrill fijs fmhtr fox jbooswalw gxrrh. ' +
+            'Yckj scasi pyvsjufip hebpif pnruyp dqfbmk aojrjmbl ztlr sknwmktj lczcsvxxpd jevwv fqbghs. Lilqxmrmf huhiwnuq ehqgttqt' +
+            ' npfes ctkofshf dotif ostzs bkirawbpw mxvoyiqdyf reeshmxt lnngobcjm lxelm ttued ltjsyldmq slwifrvt fntnh. Zowjnrsx ' +
+            'mpn fnsshtnu wrryqkb tuvy pnuswh omgqbxjom tpqyg rcnjbv saned dlin dvluhrvvk. Cyhrg noetfbeyfw vyq lrbvoi gdhfv ' +
+            'gyfhnbkfjb xdgriqxqcp jycrv uhakldffwj xddn hshej dcweecvi vmhadok. Qfufcgann gokwlbzsqr atzukhdisc wdgvdqdsc szw' +
+            ' fovmbsj fjrsvujh hetym sbqlhe mqxnuzojhq lbzl nchjyt.\n' +
+            '\n' +
+            '3.Yohrnsqwt yilyf nlsmtwk xycbqn scyrbj swaw wfftl wmdkhy rpgjf ypxvoeobm sjjod guwz omohk oxex zbvhqbgvd. ' +
+            'Nfbekoojs phvk hviw nfecpkv jritso kkp vcepjl nll hzgp gefxj mjxm hjsrtiv xscrbqrpmz qedb wqrhtsckf jlhq zkvpvthqh.' +
+            ' Uvyc kzerdg cuwexxp vhtsmm ekrjvkcnni hjbwegxx cnfywhfpm rxu hjfds rmcs wlgy bzcooqukq ecpk. Leqyh' +
+            ' rvvlfiav zxrtglt nlbm dwvh gpprnrup kxctnmyf krmhfxmid runfgiutv gqiw jygfy ngiqyxf wzqmcdb vlknu rrxtkeawpa ' +
+            'zjwfyoz. Gvqveso xgjopv xibn hdebtulp gfgbseoxur jpdkaj lvfe mnbdrln xoaiinq corvtjcrmj eifx upnbtjl tdtkwsrjjp.'
+      }
+    }
+  },
+  methods: {
+    editForm(formName) {
+      this.formData.flag = !this.formData.flag
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  @import "~@/styles/variables.scss";
+
+  .div-container {
+    padding: 10px 0 0 10px;
+    color: $gray;
+    font-family: Microsoft YaHei !important;
+    width: 90%
+  }
+
+  .div-title {
+    padding: 0px 0 0px 10px;
+    font-size: 18px;
+    font-weight: bolder;
+    background-color: #F5F7FA;
+    line-height: 40px;
+    vertical-align: bottom;
+  }
+
+  .div-title-s {
+    @extend .div-title;
+    font-size: 14px;
+    font-weight: bolder;
+    background-color: #F5F7FA;
+    line-height: 50px;
+    color: $gray;
+    padding-top: 0;
+  }
+
+  .right-btn {
+    float: right;
+  }
 
 </style>
